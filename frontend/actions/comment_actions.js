3bd1de2ed3f1bee171b4238b401504fa0a67bafa
Maxbryan Cosmosse <mcosmosse@gmail.com>
2021-03-09 17:17:33 -0800

    create comment utils and actions

---
+++ frontend/actions/comment_actions.js
@@ -0,0 +1,54 @@
+import * as Util from '../utils/comments_util';
+
+export const RECEIVE_COMMENT = "RECEIVE_COMMENT";
+export const RECEIVE_COMMENTS = "RECEIVE_COMMENTS";
+export const REMOVE_COMMENT = "REMOVE_COMMENT";
+
+// regular actions
+
+export const receiveComment = (comment) => ({
+    type: RECEIVE_COMMENT,
+    comment
+});
+
+export const receiveComments = (comments) => ({
+    type: RECEIVE_COMMENTS,
+    comments
+});
+
+export const removeComment = (comment) => ({
+    type: REMOVE_COMMENT,
+    comment
+})
+
+// thunk action creators
+
+export const fetchComment = id => dispatch => (
+    Util.fetchComment(id).then(comment => (
+        dispatch(receiveComment(comment))
+    ))
+);
+
+export const fetchComments = () => dispatch => (
+    Util.fetchComments().then(comments => (
+        dispatch(receiveComments(comments))
+    ))
+);
+
+export const deleteComment = id => dispatch => (
+    Util.deleteComment(id).then(comment => (
+        dispatch(removeComment(comment))
+    ))
+);
+
+export const createComment = comment => dispatch => (
+    Util.createComment(comment).then(newChapter => (
+        dispatch(receiveComment(newChapter))
+    ))
+);
+
+export const updateComment = comment => dispatch => (
+    Util.updateComment(comment).then(newChapter => (
+        dispatch(receiveComment(newChapter))
+    ))
+);
